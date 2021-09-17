bdbd387bf1941e0fec20e8c5d7523fc967e58ecd
Stefan Berggren <nsg@nsg.cc>
2021-09-18 00:47:17 +0200

    Added night vision mode

--- analyze.py
+++ analyze.py
@@ -33,6 +33,11 @@ def get_norm_frame(buffer, offset):
     return frame
 
 
+def avg_color(frame):
+    avgcol_f = numpy.average(frame, axis=0)
+    return numpy.average(avgcol_f, axis=0)
+
+
 def diff_frames(f1, f2, out_frame):
 
     norm_frame1 = get_norm_frame(CAPTURE_BUFFER, f1)
@@ -55,14 +60,17 @@ def diff_frames(f1, f2, out_frame):
         crop_frame1 = frame1[y * 4 : y * 4 + h * 4, x * 4 : x * 4 + w * 4]
         crop_frame2 = frame2[y * 4 : y * 4 + h * 4, x * 4 : x * 4 + w * 4]
 
-        avg_color_per_row1 = numpy.average(crop_frame1, axis=0)
-        avg_color1 = numpy.average(avg_color_per_row1, axis=0)
-        avg_color_per_row2 = numpy.average(crop_frame2, axis=0)
-        avg_color2 = numpy.average(avg_color_per_row2, axis=0)
+        if not NIGHT_VISION:
+            avg_color1 = avg_color(crop_frame1)
+            avg_color2 = avg_color(crop_frame2)
 
-        color_channel_1 = avg_color1[0] - avg_color2[0]
-        color_channel_2 = avg_color1[1] - avg_color2[1]
-        color_channel_3 = avg_color1[2] - avg_color2[2]
+            color_channel_1 = avg_color1[0] - avg_color2[0]
+            color_channel_2 = avg_color1[1] - avg_color2[1]
+            color_channel_3 = avg_color1[2] - avg_color2[2]
+        else:
+            color_channel_1 = 255
+            color_channel_2 = 255
+            color_channel_3 = 255
 
         color_channel_diff_tr = 25
         if (
@@ -80,8 +88,8 @@ def diff_frames(f1, f2, out_frame):
             if y > 10:
                 movement = True
                 # debug
-                #im_v = cv2.vconcat([crop_frame1, crop_frame2])
-                #cv2.imwrite(f"{PATH}/debug.jpg", im_v)
+                # im_v = cv2.vconcat([crop_frame1, crop_frame2])
+                # cv2.imwrite(f"{PATH}/debug.jpg", im_v)
         else:
             cv2.rectangle(
                 out_frame,
@@ -99,6 +107,12 @@ CAPTURE_BUFFER_SIZE = len(CAPTURE_BUFFER)
 
 frame = CAPTURE_BUFFER[0]
 
+avgcol = avg_color(frame)
+if int(avgcol[0]) == int(avgcol[1]) and int(avgcol[0]) == int(avgcol[2]):
+    NIGHT_VISION = True
+else:
+    NIGHT_VISION = False
+
 diffed_segments = 0
 for f1 in range(0, CAPTURE_BUFFER_SIZE, 30):
     f2 = f1 + 14
@@ -110,9 +124,11 @@ for f1 in range(0, CAPTURE_BUFFER_SIZE, 30):
     if frame_diff:
         diffed_segments += 1
 
-print(f"{FILEPATH}: {CAPTURE_BUFFER_SIZE} frames, {diffed_segments} movements")
+print(
+    f"{FILEPATH}: {CAPTURE_BUFFER_SIZE} frames, {diffed_segments} move, night:{NIGHT_VISION}"
+)
 
-if diffed_segments > 3:
+if diffed_segments > 3 or NIGHT_VISION:
     cv2.imwrite(f"{PATH}/match.jpg", frame)
     sys.exit(0)
 
