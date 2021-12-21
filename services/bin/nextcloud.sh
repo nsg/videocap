a4df2b3e1748682c9d63100fba54ac3a566c39ed
Stefan Berggren <nsg@nsg.cc>
2021-12-22 00:35:28 +0100

    Add a few friendly messages

--- services/bin/nextcloud.sh
+++ services/bin/nextcloud.sh
@@ -11,11 +11,13 @@ while [ 1 ]; do
         SCORE_NAME="$(echo $CAMERA_FILTERED | sed 's/[^a-z0-9]/_/g')"
         STORDIR="$SNAP_COMMON/$SCORE_NAME"
 
+        echo "Process $STORDIR"
         cd $STORDIR
         for f in *.avi; do
             MODE="$(stat --format=%a $f)"
             SIZE="$(stat --format=%s $f)"
             if [[ $MODE == ??4 ]] && [[ $SIZE -gt 32768 ]]; then
+                echo "I will upload $f"
                 $SNAP/bin/push-nextcloud.sh "$f" && chmod o-r $f
             fi
         done
